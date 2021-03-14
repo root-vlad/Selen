b677f5937b739a3fcee24b6f90ac2ad4dea8d9de
Mashadar0 <shuvikoff2012@yandex.ru>
2021-03-14 18:42:05 +0400

    upd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,34 +15,50 @@ public class Main {
         driver.get("https://www.avito.ru/");
         driver.manage().window().maximize();
 
-        WebElement category = driver.findElement(By.xpath("//option[@data-marker=\"option(99)\"]"));
+        By byCategory = By.xpath("//option[@data-marker=\"option(99)\"]");
+        By bySearch = By.id("search");
+        By byCity = By.xpath("//div[@data-marker=\"search-form/region\"]");
+        By byCityInput = By.xpath("//input[@data-marker=\"popup-location/region/input\"]");
+        By byCitySelect = By.xpath("//ul[@data-marker=\"suggest-list\"]/li[1]");
+        By byCityConfirm = By.cssSelector("[data-marker=\"popup-location/save-button\"]");
+        By byCheckbox = By.cssSelector("[data-marker=\"delivery-filter/text\"]");
+        By bySearchConfirm = By.cssSelector("[data-marker=\"search-filters/submit-button\"]");
+        By bySort = By.cssSelector("div.sort-select-3QxXG option[data-marker=\"option(2)\"]");
+        By[] byName = new By[3];
+        By[] byPrice = new By[3];
+        for (int i = 0; i < 3; i++) {
+            byName[i] = By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + (i + 1) + "]//h3");
+            byPrice[i] = By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + (i + 1) + "]//span[@class=\"price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo\"]");
+        }
+
+        WebElement category = driver.findElement(byCategory);
         category.click();
-        WebElement search = driver.findElement(By.id("search"));
+        WebElement search = driver.findElement(bySearch);
         search.sendKeys("Принтер");
-        WebElement city = driver.findElement(By.xpath("//div[@data-marker=\"search-form/region\"]"));
+        WebElement city = driver.findElement(byCity);
         city.click();
-        WebElement cityInput = driver.findElement(By.xpath("//input[@data-marker=\"popup-location/region/input\"]"));
+        WebElement cityInput = driver.findElement(byCityInput);
         cityInput.sendKeys("Владивосток");
         Thread.sleep(2000);
-        WebElement citySelect = driver.findElement(By.xpath("//ul[@data-marker=\"suggest-list\"]/li[1]"));
+        WebElement citySelect = driver.findElement(byCitySelect);
         citySelect.click();
 
-        WebElement cityConfirm = driver.findElement(By.cssSelector("[data-marker=\"popup-location/save-button\"]"));
+        WebElement cityConfirm = driver.findElement(byCityConfirm);
         cityConfirm.click();
-        WebElement checkbox = driver.findElement(By.cssSelector("[data-marker=\"delivery-filter/text\"]"));
+        WebElement checkbox = driver.findElement(byCheckbox);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox);
-        if (!checkbox.isSelected()){
+        if (!checkbox.isSelected()) {
             checkbox.click();
         }
-        WebElement searchConfirm = driver.findElement(By.cssSelector("[data-marker=\"search-filters/submit-button\"]"));
+        WebElement searchConfirm = driver.findElement(bySearchConfirm);
         searchConfirm.click();
-        WebElement sort = driver.findElement(By.cssSelector("div.sort-select-3QxXG option[data-marker=\"option(2)\"]"));
+        WebElement sort = driver.findElement(bySort);
         sort.click();
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-        for (int i = 1; i <= 3; i++) {
-            WebElement name = driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//h3"));
+        for (int i = 0; i < 3; i++) {
+            WebElement name = driver.findElement(byName[i]);
             System.out.println(name.getText());
-            WebElement price = driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//span[@class=\"price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo\"]"));
+            WebElement price = driver.findElement(byPrice[i]);
             System.out.println(price.getText());
         }
 
