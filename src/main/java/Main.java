13b62fcb2219c9232ef050514c4780205d72a4c5
Mashadar0 <shuvikoff2012@yandex.ru>
2021-03-14 10:08:53 +0400

    upd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,31 +9,41 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.avito.ru/");
         driver.manage().window().maximize();
 
-        driver.findElement(By.cssSelector("[data-marker=\"option(99)\"]")).click();
-        driver.findElement(By.id("search")).sendKeys("Принтер");
-        driver.findElement(By.cssSelector("[data-marker=\"search-form/region\"]")).click();
-        driver.findElement(By.cssSelector("[data-marker=\"popup-location/region/input\"]")).sendKeys("Владивосток");
+        WebElement category = driver.findElement(By.xpath("//option[@data-marker=\"option(99)\"]"));
+        category.click();
+        WebElement search = driver.findElement(By.id("search"));
+        search.sendKeys("Принтер");
+        WebElement city = driver.findElement(By.xpath("//div[@data-marker=\"search-form/region\"]"));
+        city.click();
+        WebElement cityInput = driver.findElement(By.xpath("//input[@data-marker=\"popup-location/region/input\"]"));
+        cityInput.sendKeys("Владивосток");
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//ul[@data-marker=\"suggest-list\"]/li[1]")).click();
+        WebElement citySelect = driver.findElement(By.xpath("//ul[@data-marker=\"suggest-list\"]/li[1]"));
+        citySelect.click();
 
-        driver.findElement(By.cssSelector("[data-marker=\"popup-location/save-button\"]")).click();
-        WebElement element = driver.findElement(By.cssSelector("[data-marker=\"delivery-filter/text\"]"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
-        if (!driver.findElement(By.cssSelector("[data-marker=\"delivery-filter/text\"]")).isSelected()){
-            driver.findElement(By.cssSelector("[data-marker=\"delivery-filter/text\"]")).click();
+        WebElement cityConfirm = driver.findElement(By.cssSelector("[data-marker=\"popup-location/save-button\"]"));
+        cityConfirm.click();
+        WebElement checkbox = driver.findElement(By.cssSelector("[data-marker=\"delivery-filter/text\"]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", checkbox);
+        if (!checkbox.isSelected()){
+            checkbox.click();
         }
-        driver.findElement(By.cssSelector("[data-marker=\"search-filters/submit-button\"]")).click();
-        driver.findElement(By.cssSelector("div.sort-select-3QxXG option[data-marker=\"option(2)\"]")).click();
+        WebElement searchConfirm = driver.findElement(By.cssSelector("[data-marker=\"search-filters/submit-button\"]"));
+        searchConfirm.click();
+        WebElement sort = driver.findElement(By.cssSelector("div.sort-select-3QxXG option[data-marker=\"option(2)\"]"));
+        sort.click();
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
         for (int i = 1; i <= 3; i++) {
-            System.out.println(driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//h3")).getText());
-            System.out.println(driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//span[@class=\"price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo\"]")).getText());
+            WebElement name = driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//h3"));
+            System.out.println(name.getText());
+            WebElement price = driver.findElement(By.xpath("//div[@data-marker=\"catalog-serp\"]//div[@class=\"iva-item-root-G3n7v photo-slider-slider-3tEix iva-item-list-2_PpT items-item-1Hoqq items-listItem-11orH js-catalog-item-enum\"][" + i + "]//span[@class=\"price-text-1HrJ_ text-text-1PdBw text-size-s-1PUdo\"]"));
+            System.out.println(price.getText());
         }
 
     }
